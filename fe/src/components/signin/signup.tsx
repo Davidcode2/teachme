88a7b8e494603d92cb6918c7ee5b96179a9dfed8
Jakob Lingel <jlingel68@gmail.com>
2024-03-06 20:25:51 +0100

    Styling signup

--- fe/src/components/signin/signup.tsx
+++ fe/src/components/signin/signup.tsx
@@ -1,18 +1,22 @@
+import ArrowIcon from '../../assets/icons/icons8-logout-50.png';
+
 function SignUpForm() {
   return (
     <>
       <div className="h-[80vh] xl:grid grid-cols-[40vw_60vw] justify-center items-center">
-      <div>
-      <h1 className="m-4 md:m-20 text-9xl font-extrabold">SIGN UP</h1>
-      </div>
-        <div className="border border-slate-400 rounded-xl p-10 md:p-20 shadow-md xl:w-[30vw] md:m-20 m-4">
+        <div>
+          <h1 className="m-4 md:m-20 text-9xl font-extrabold">SIGN UP</h1>
+        </div>
+        <div className="border border-slate-400 rounded-xl shadow-md xl:w-[30vw] md:m-20 m-4">
           <form className="flex flex-col">
-            <div className="grid grid-cols-[100px_1fr]">
+            <button className="ml-auto p-4 invisible"><img src={ArrowIcon} width="30" alt="" /></button>
+            <div className="grid grid-cols-[100px_1fr] px-10 md:px-20 py-4">
               <label className="p-2" htmlFor="email">E-Mail</label>
-              <input className="p-2 border-b" id="email" type="email" name="email" />
+              <input className="p-2 rounded-md border-b" id="email" type="email" name="email" />
               <label className="p-2" htmlFor="password">Password</label>
-              <input className="p-2 border-b" id="password" type="password" name="password" />
+              <input className="p-2 rounded-md border-b" id="password" type="password" name="password" />
             </div>
+            <button className="ml-auto p-4"><img src={ArrowIcon} width="30" alt="" /></button>
           </form>
         </div>
       </div>
