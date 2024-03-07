0cfb69853d9da730982ef534d92df3749df79509
Jakob Lingel <jlingel68@gmail.com>
2024-03-07 13:36:01 +0100

    Adjust styling of material card

--- fe/src/components/card/card.tsx
+++ fe/src/components/card/card.tsx
@@ -5,9 +5,9 @@ import Material from "../../DTOs/material.ts"
 
 function Card({material}): JSX.Element {
   return (
-    <div className="m-10 rounded-lg bg-slate-400 flex flex-col md:flex-row shadow-lg">
-      <img src={sampleImage} className="w-[400px]" alt="SampleImage" />
-      <div className="p-10 flex flex-col gap-4 overflow-auto">
+    <div className="m-10 rounded-lg border-slate-100 border flex flex-col md:flex-row shadow-lg">
+      <img src={sampleImage} className="w-[400px] rounded-lg" alt="SampleImage" />
+      <div className="p-10 flex flex-col gap-4 overflow-auto border-l border-slate-100">
         <div className="flex flex-col">
           <div className="text-2xl">{material.title}</div>
           <div>Description Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet.</div>
