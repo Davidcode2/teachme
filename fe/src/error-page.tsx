d288f73e7297dbe550475ae7b105051cee8fa53e
Jakob Lingel <jlingel68@gmail.com>
2024-03-13 20:30:48 +0100

    Style error page

--- fe/src/error-page.tsx
+++ fe/src/error-page.tsx
@@ -6,9 +6,11 @@ export default function ErrorPage() {
 
   return (
     <div className="flex justify-center place-items-center h-screen flex-col gap-10">
-      <h1 className="text-9xl text-slate-600 font-extrabold">404</h1>
+      <h1 className="text-9xl text-slate-600 font-extrabold">Ups</h1>
       <img src={catSticker} alt="" width="200"/>
-      <p className="text-2xl text-slate-800">{error.statusText || error.message} </p>
+      <h1 className="text-4xl text-slate-500 font-bold">Ein unerwarteter Fehler ist aufgetreten</h1>
+      <p className="text-4xl text-slate-500 font-bold">{error.statusText || error.message} </p>
+    <a className="text-slate-400" href="https://www.freepik.com/free-vector/sticker-design-with-little-cat-isolated_18987720.htm#fromView=search&page=1&position=38&uuid=ef7ff577-d622-4bd8-8061-7dc8d84cbf83">Image by brgfx on Freepik</a>
     </div>
   );
 
