9d63734707452976eecef115dd26f31f2c8080f3
Jakob Lingel <jlingel68@gmail.com>
2024-03-22 10:25:35 +0100

    Extract validation to component

---
+++ fe/src/components/signin/passwordValidation.tsx
@@ -0,0 +1,86 @@
+import { useEffect, useState } from "react";
+import CheckMarkIcon from '../../assets/icons/icons8-checkmark-48.png';
+
+export default function PasswordValidation({ password }) {
+  const [passwordLengthRequirementMet, setPasswordLengthRequirementMet] = useState(false);
+  const [passwordUppercaseRequirementMet, setPasswordUppercaseRequirementMet] = useState(false);
+  const [passwordLowercaseRequirementMet, setPasswordLowercaseRequirementMet] = useState(false);
+  const [passwordNumberRequirementMet, setPasswordNumberRequirementMet] = useState(false);
+
+  useEffect(() => {
+    checkPasswordRequirements();
+  }, [password]);
+
+  const checkPasswordRequirements = () => {
+    console.log(password);
+    let current = password;
+    checkPasswordLengthRequirement(current);
+    checkPasswordUpperCaseRequirement(current);
+    checkPasswordLowerCaseRequirement(current);
+    checkPasswordNumberRequirement(current);
+  }
+
+  const checkPasswordNumberRequirement = (password: string) => {
+    console.log(password.match(/.*[\d].*/));
+    if (password.match(/.*[\d].*/)) {
+      setPasswordNumberRequirementMet(true);
+    } else {
+      setPasswordNumberRequirementMet(false);
+    }
+  }
+
+  const checkPasswordLowerCaseRequirement = (password: string) => {
+    if (password.match(/.*[a-z].*/)) {
+      setPasswordLowercaseRequirementMet(true);
+    } else {
+      setPasswordLowercaseRequirementMet(false);
+    }
+  }
+
+  const checkPasswordUpperCaseRequirement = (password: string) => {
+    if (password.match(/.*[A-Z].*/)) {
+      setPasswordUppercaseRequirementMet(true);
+    } else {
+      setPasswordUppercaseRequirementMet(false);
+    }
+  }
+
+  const checkPasswordLengthRequirement = (password: string) => {
+    if (password.length >= 6) {
+      setPasswordLengthRequirementMet(true);
+    } else {
+      setPasswordLengthRequirementMet(false);
+    }
+  }
+
+  return (
+    <div className="col-start-2 mt-2">
+      <ul className="flex sm:flex-row flex-col text-xs gap-x-2">
+        <li className={passwordNumberRequirementMet ? "text-green-600" : "text-gray-400 grayscale"}>
+          <div className="flex items-center gap-1">
+            <img src={CheckMarkIcon} width="15" alt="" />
+            Zahl
+          </div>
+        </li>
+        <li className={passwordUppercaseRequirementMet ? "text-green-600" : "text-gray-400 grayscale"}>
+          <div className="flex items-center gap-1">
+            <img src={CheckMarkIcon} width="15" alt="" />
+            Großbuchstabe
+          </div>
+        </li>
+        <li className={passwordLowercaseRequirementMet ? "text-green-600" : "text-gray-400 grayscale"}>
+          <div className="flex items-center gap-1">
+            <img src={CheckMarkIcon} width="15" alt="" />
+            Kleinbuchstabe
+          </div>
+        </li>
+        <li className={passwordLengthRequirementMet ? "text-green-600" : "text-gray-400 grayscale"}>
+          <div className="flex items-center gap-1">
+            <img src={CheckMarkIcon} width="15" alt="" />
+            &ge; 6 Zeichen
+          </div>
+        </li>
+      </ul>
+    </div>
+  )
+}
--- fe/src/components/signin/signup.tsx
+++ fe/src/components/signin/signup.tsx
@@ -3,53 +3,13 @@ import ChevronIcon from '../../assets/icons/icons8-chevron-24.png';
 import ArrowIcon from '../../assets/icons/icons8-logout-50.png';
 import CheckMarkIcon from '../../assets/icons/icons8-checkmark-48.png';
 import { useState } from 'react';
+import PasswordValidation from './passwordValidation';
 
 function SignUpForm() {
-  const [passwordLengthRequirementMet, setPasswordLengthRequirementMet] = useState(false);
-  const [passwordUppercaseRequirementMet, setPasswordUppercaseRequirementMet] = useState(false);
-  const [passwordLowercaseRequirementMet, setPasswordLowercaseRequirementMet] = useState(false);
-  const [passwordNumberRequirementMet, setPasswordNumberRequirementMet] = useState(false);
+  const [password, setPassword] = useState('');
 
   const checkPasswordRequirements = (e) => {
-    console.log(e.target.value);
-    let current = e.target.value;
-    checkPasswordLengthRequirement(current);
-    checkPasswordUpperCaseRequirement(current);
-    checkPasswordLowerCaseRequirement(current);
-    checkPasswordNumberRequirement(current);
-  }
-
-  const checkPasswordNumberRequirement = (password: string) => {
-    console.log(password.match(/.*[\d].*/));
-    if (password.match(/.*[\d].*/)) {
-      setPasswordNumberRequirementMet(true);
-    } else {
-      setPasswordNumberRequirementMet(false);
-    }
-  }
-
-  const checkPasswordLowerCaseRequirement = (password: string) => {
-    if (password.match(/.*[a-z].*/)) {
-      setPasswordLowercaseRequirementMet(true);
-    } else {
-      setPasswordLowercaseRequirementMet(false);
-    }
-  }
-
-  const checkPasswordUpperCaseRequirement = (password: string) => {
-    if (password.match(/.*[A-Z].*/)) {
-      setPasswordUppercaseRequirementMet(true);
-    } else {
-      setPasswordUppercaseRequirementMet(false);
-    }
-  }
-
-  const checkPasswordLengthRequirement = (password: string) => {
-    if (password.length >= 8) {
-      setPasswordLengthRequirementMet(true);
-    } else {
-      setPasswordLengthRequirementMet(false);
-    }
+    setPassword(e.target.value);
   }
 
   return (
@@ -66,35 +26,8 @@ function SignUpForm() {
               <label className="p-2" htmlFor="email">E-Mail</label>
               <input className="p-2 rounded-md border-b" id="email" type="email" name="email" maxLength="80" required />
               <label className="p-2" htmlFor="password">Password</label>
-              <input onChange={checkPasswordRequirements} className="p-2 rounded-md border-b" id="password" minLength="6" type="password" name="password" maxLength="20" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$" required />
-              <div className="col-start-2 mt-2">
-                <ul className="flex sm:flex-row flex-col text-xs gap-x-2">
-                  <li className={passwordNumberRequirementMet ? "text-green-600" : "text-gray-400 grayscale"}>
-                    <div className="flex items-center gap-1">
-                      <img src={CheckMarkIcon} width="15" alt="" />
-                      Zahl
-                    </div>
-                  </li>
-                  <li className={passwordUppercaseRequirementMet ? "text-green-600" : "text-gray-400 grayscale"}>
-                    <div className="flex items-center gap-1">
-                      <img src={CheckMarkIcon} width="15" alt="" />
-                      Großbuchstabe
-                    </div>
-                  </li>
-                  <li className={passwordLowercaseRequirementMet ? "text-green-600" : "text-gray-400 grayscale"}>
-                    <div className="flex items-center gap-1">
-                      <img src={CheckMarkIcon} width="15" alt="" />
-                      Kleinbuchstabe
-                    </div>
-                  </li>
-                  <li className={passwordLengthRequirementMet ? "text-green-600" : "text-gray-400 grayscale"}>
-                    <div className="flex items-center gap-1">
-                      <img src={CheckMarkIcon} width="15" alt="" />
-                      &gt; 8 Zeichen
-                    </div>
-                  </li>
-                </ul>
-              </div>
+              <input onChange={checkPasswordRequirements} className="p-2 rounded-md border-b" id="password" minLength="6" type="password" name="password" maxLength="20" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$" required />
+              <PasswordValidation password={password} />
             </div>
             <button type="submit" className="ml-auto p-4"><img src={ArrowIcon} width="30" alt="" /></button>
           </Form>
