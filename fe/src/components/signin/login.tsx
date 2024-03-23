a22b55201e66c23c7eaf5cb6baf7c7ddee9f7b0e
Jakob Lingel <jlingel68@gmail.com>
2024-03-23 18:21:25 +0100

    Paper plane on login

--- fe/src/components/signin/login.tsx
+++ fe/src/components/signin/login.tsx
@@ -1,27 +1,82 @@
 import ArrowIcon from '../../assets/icons/icons8-logout-50.png';
+import PaperPlane from '../../assets/icons/icons8-paper-plane-64.png';
 import ChevronIcon from '../../assets/icons/icons8-chevron-24.png';
-import { Form, Link } from "react-router-dom";
+import { Form, Link, useActionData, useNavigation } from "react-router-dom";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { useRef, useState } from 'react';
 
 function LoginForm() {
+  const [showPlane, setShowPlane] = useState(false);
+  const navigation = useNavigation();
+  const loginSuccess = useActionData();
+  const tl = useRef();
+  gsap.registerPlugin(MotionPathPlugin)
+
+  console.log(loginSuccess);
+
+  useGSAP(() => {
+    tl.current = gsap.timeline({ paused: true })
+      .to("#paperPlane",
+        {
+          duration: 4,
+          motionPath: {
+            relative: true,
+            path: [{ x: 500, y: -400, }, { x: -400, y: -100 }, { x: 400, y: 400 }, { x: 500, y: 0 }],
+            autoRotate: true,
+            curviness: 2,
+          }
+        });
+  });
+
+  const toggleTimeline = (() => {
+    setShowPlane(true);
+    tl.current.play();
+  });
+
+  if (loginSuccess === false) {
+    console.log("login failed");
+    tl.current.to("#paperPlane",
+      {
+        duration: 1,
+        motionPath: {
+          path: [{ x: 100, y: 100 }],
+          relative: true,
+          autoRotate: true,
+          curviness: 2,
+        }
+      }, "-=1");
+  }
 
   return (
     <>
-    <Link to="/materials"><img className="p-4 md:px-20" src={ChevronIcon} alt=""/></Link>
-      <div className="lg:h-[80vh] xl:grid grid-cols-[40vw_60vw] justify-center items-center">
+      <Link to="/materials"><img className="p-4 md:px-20" src={ChevronIcon} alt="" /></Link>
+      <div className="lg:h-[80vh] xl:grid grid-cols-2 justify-center items-center">
         <div>
           <h1 className="m-4 md:m-20 text-7xl md:text-9xl font-extrabold">Schön, dass du wieder da bist</h1>
         </div>
         <div className="border border-slate-400 rounded-xl shadow-md xl:w-[30vw] md:m-20 m-4">
-          <Form method="post" className="flex flex-col">
+          <Form onSubmit={toggleTimeline} method="post" className="flex flex-col">
             <button className="ml-auto p-4 invisible"><img src={ArrowIcon} width="30" alt="" /></button>
             <div className="grid grid-cols-[.2fr_1fr] gap-y-1 px-4 sm:px-10 2xl:px-20 py-4">
               <label className="p-2" htmlFor="email">E-Mail</label>
-              <input className="p-2 rounded-md border-b" id="email" type="email" name="email" maxLength="80" required/>
+              <input className="p-2 rounded-md border-b" id="email" type="email" name="email" maxLength="80" required />
               <label className="p-2" htmlFor="password">Password</label>
               <input className="p-2 rounded-md border-b" id="password" type="password" name="password" minLength="6" maxLength="20" required />
             </div>
+            <div className="flex justify-center text-sm text-red-400">
+              <div className={loginSuccess === false ? "flex" : "hidden"}>
+                Ups, das hat nicht geklappt
+              </div>
+            </div>
             <button type="submit" className="ml-auto p-4"><img src={ArrowIcon} width="30" alt="" /></button>
           </Form>
+        </div>
+        <div className="flex">
+          <div className={showPlane ? "block" : "hidden"}>
+            <img id="paperPlane" className={loginSuccess === false ? "" : ""} src={PaperPlane} alt="Paper Plane" />
+          </div>
         </div>
       </div>
       <div className="flex justify-center my-10">
