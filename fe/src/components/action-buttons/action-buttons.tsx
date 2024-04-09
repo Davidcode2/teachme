d6b29389649a360eb5540aca5dc5c579c46c4b57
Jakob Lingel <jlingel68@gmail.com>
2024-04-09 11:42:00 +0200

    Loading indicator for adding material

--- fe/src/components/action-buttons/action-buttons.tsx
+++ fe/src/components/action-buttons/action-buttons.tsx
@@ -2,8 +2,11 @@ import { Link } from 'react-router-dom';
 import addToShoppingCartIcon from '../../assets/icons/icons8-add-shopping-cart-50.png';
 import arrowIcon from '../../assets/icons/icons8-arrow-50.png';
 import { useCartStore, useUserStore } from '../../store';
+import { useState } from 'react';
 
 function ActionButtons({ id, path }) {
+  const [loading, setLoading] = useState(false);
+
   const { user } = useUserStore();
   if (!user) return (
     <div className="flex">
@@ -14,6 +17,7 @@ function ActionButtons({ id, path }) {
   )
 
   const addToShoppingCart = async () => {
+    setLoading(true);
     const res = await fetch('/api/cart', {
       method: 'POST',
       headers: {
@@ -22,17 +26,19 @@ function ActionButtons({ id, path }) {
       body: JSON.stringify({ 'userId': user.id, 'materialId': id })
     });
     const data = await res.json();
+    setLoading(false);
     useCartStore.setState({ cart: data });
   }
 
   return (
     <>
-      <div className="flex">
+      <div className="flex gap-x-2">
         <div className="hidden">rating</div>
         <div className="hidden">downloads</div>
         <div className="hover:cursor-pointer hover:bg-gray-100 rounded-full">
           <img className="" onClick={addToShoppingCart} src={addToShoppingCartIcon} width="30" alt="" />
         </div>
+        { loading ? <div className="animate-spin">|</div> : <></>}
         {path ?
           <div className="hover:cursor-pointer hover:bg-gray-100 rounded-full">
             <a href={`/api/materials/download?id=${id}`} download={id}><img className="rotate-90" src={arrowIcon} width="30" alt="" /></a>
