97bd2bb4a50b7a6a6f734e51d31027c8fd3a1bc0
Jakob Lingel <jlingel68@gmail.com>
2024-04-10 10:21:36 +0200

    Fix sidebar animation for small screens

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -5,7 +5,6 @@ import Sidebar from './components/sidebar/sidebar'
 import SpinnerGif from './assets/icons/icons8-spinner.gif'
 import { useEffect, useState } from 'react';
 import { useAccessTokenStore, useUserStore, useGlobalLoadingStore, useSidebarStore } from './store';
-import { gsap } from 'gsap';
 
 function App(): JSX.Element {
   let loading = useGlobalLoadingStore((state) => state.loading);
@@ -33,22 +32,13 @@ function App(): JSX.Element {
 
   }, []);
 
-  useSidebarStore.subscribe(() => {
-    gsap.set('.sidebar', { x: 300 });
-    gsap.to('.sidebar', { x: 0, duration: 1 });
-  });
-
   return (
     <div>
       <div className="text-5xl font-bold fixed top-1/2 left-1/2">
         {loading ? <div><img src={SpinnerGif} alt="" width="60" /></div> : <></>}
       </div>
       <div className={loading ? "blur-sm" : ""}>
-        <div className="sidebar">
-          {sidebarShown ?
-            <Sidebar></Sidebar>
-            : <></>}
-        </div>
+        <Sidebar></Sidebar>
         <Header></Header>
         <Outlet />
       </div>
--- fe/src/components/sidebar/sidebar.tsx
+++ fe/src/components/sidebar/sidebar.tsx
@@ -1,22 +1,33 @@
 import hamburgerIcon from "../../assets/icons/icons8-hamburger-50.png"
-import { useSidebarStore} from "../../store";
+import { useSidebarStore } from "../../store";
 import Cart from "../cart/cart";
 import UserMenu from "../userMenu";
+import { gsap } from 'gsap';
 
 export default function Sidebar() {
   const toggleSidebarShown = useSidebarStore((state) => state.toggleSidebar);
+  let sidebarShown = useSidebarStore((state) => state.isShown);
+
+  useSidebarStore.subscribe(() => {
+    gsap.set('.sidebar', { x: 300 });
+    gsap.to('.sidebar', { x: 0, duration: 1 });
+  });
 
   return (
-    <div className="p-4 md:fixed bg-white shadow-md overflow-y-auto md:w-[500px] md:right-0 h-screen z-50">
-      <div className="flex flex-col">
-        <div className="my-[6px] self-end">
-          <div className="flex items-center gap-2">
-            <UserMenu />
-            <button onClick={toggleSidebarShown}><img src={hamburgerIcon} width="30" alt="" /></button>
+    <div className="sidebar fixed right-0 w-screen md:w-[500px]">
+      {sidebarShown ?
+        <div className="p-4 bg-white shadow-md overflow-y-auto md:w-[500px] h-screen z-50">
+          <div className="flex flex-col">
+            <div className="my-[6px] self-end">
+              <div className="flex items-center gap-2">
+                <UserMenu />
+                <button onClick={toggleSidebarShown}><img src={hamburgerIcon} width="30" alt="" /></button>
+              </div>
+            </div>
+            <Cart />
           </div>
         </div>
-        <Cart />
-      </div>
+        : <></>}
     </div>
   )
 }
