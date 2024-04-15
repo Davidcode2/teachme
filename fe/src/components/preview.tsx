4f1e9a2c36b200f4559cb17708f914aeb0f53b0a
Jakob Lingel <jlingel68@gmail.com>
2024-04-15 11:46:53 +0200

    Responsive preview

--- fe/src/components/preview.tsx
+++ fe/src/components/preview.tsx
@@ -1,14 +1,13 @@
 import sampleImage from "../assets/exampleMaterialThumbnail.png"
 export default function Preview({ material, images }) {
 
-  console.log(material);
   return (
     <div className="fixed top-0 h-screen w-screen backdrop-blur-sm">
-      <div className="flex justify-center items-center m-20">
+      <div className="flex justify-center items-center h-full">
         {!material
           ? <div>lädt...</div>
           :
-          <div className="flex bg-white p-10 border rounded-lg shadow-lg gap-5">
+          <div className="flex flex-col sm:flex-row bg-white p-10 border rounded-lg shadow-lg gap-5 mx-10 lg:mx-0">
             <div className="overflow-scroll h-[75vh]">
               {images.map((img, index) => <img src={img} alt="" key={index} />)}
             </div>
