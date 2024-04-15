80f12fed5781ff713f61dfb93a40624873fdbdff
Jakob Lingel <jlingel68@gmail.com>
2024-04-15 15:45:02 +0200

    Use && for conditional rendering

--- fe/src/components/preview.tsx
+++ fe/src/components/preview.tsx
@@ -1,15 +1,14 @@
-import sampleImage from "../assets/exampleMaterialThumbnail.png"
 export default function Preview({ material, images }) {
+  const _images = images.map((img, index) => <img src={img} alt="" key={index} />)
 
   return (
     <div className="fixed top-0 h-screen w-screen backdrop-blur-sm">
       <div className="flex justify-center items-center h-full">
-        {!material
-          ? <div>lädt...</div>
-          :
+        {!material && <div>lädt...</div>}
+        {material &&
           <div className="flex flex-col sm:flex-row bg-white p-10 border rounded-lg shadow-lg gap-5 mx-10 lg:mx-0">
             <div className="overflow-scroll h-[75vh]">
-              {images.map((img, index) => <img src={img} alt="" key={index} />)}
+              {_images}
             </div>
             <div>
               {material.material.title}
