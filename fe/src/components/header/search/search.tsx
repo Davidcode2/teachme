77e246603f1296a3714b576199abb0973d57530f
Jakob Lingel <jlingel68@gmail.com>
2024-04-25 15:01:52 +0200

    Autofocus searchbar

--- fe/src/components/header/search/search.tsx
+++ fe/src/components/header/search/search.tsx
@@ -12,7 +12,7 @@ export default function Search() {
     <div className="flex h-full w-full justify-center">
       <div className="searchBox lg:w-[800px] sm:w-[600px] self-center">
         <div className="searchBox border border-gray-200 bg-white shadow-lg rounded-lg p-10 blur-none flex flex-col">
-          <input onChange={changeSearchString} className="searchBox p-4 rounded-full border" type="text" />
+          <input onChange={changeSearchString} autoFocus={true} className="searchBox p-4 rounded-full border" type="text" />
           <ul className="searchBox flex flex-col gap-2">
             <SearchResultsPreview searchResults={searchResults} />
           </ul>
