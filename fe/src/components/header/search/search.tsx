956e217737b2a66b7df7656c9c143d35b79f6b48
Jakob Lingel <jlingel68@gmail.com>
2024-04-27 13:56:09 +0200

    Responsive search box

--- fe/src/components/header/search/search.tsx
+++ fe/src/components/header/search/search.tsx
@@ -10,7 +10,7 @@ export default function Search() {
 
   return (
     <div className="flex h-full w-full justify-center">
-      <div className="searchBox lg:w-[800px] sm:w-[600px] self-center">
+      <div className="searchBox lg:w-[800px] sm:w-[600px] w-4/5 self-center">
         <div className="z-50 searchBox border border-fuchsia-200 bg-white shadow-2xl shadow-fuchsia-900 rounded-lg p-10 blur-none flex flex-col">
           <input onChange={changeSearchString} autoFocus={true} className="border border-fuchsia-200 searchBox p-4 rounded-full focus:outline-none focus:outline-purple-300 focus:shadow-lg focus:shadow-purple-200 focus:border-none" type="text" />
           <ul className="searchBox flex flex-col gap-2">
--- fe/src/components/header/search/searchResultsPreview.tsx
+++ fe/src/components/header/search/searchResultsPreview.tsx
@@ -5,7 +5,7 @@ type SearchResultsPreviewProps = {
 export default function SearchResultsPreview({ searchResults }: SearchResultsPreviewProps) {
 
   return (
-    <div className="grid grid-cols-2 mt-4">
+    <div className="sm:grid grid-cols-2 mt-4">
       <div className="">
         {searchResults.map((el: any, index: number) => <div key={index} className="inline-block mr-2 mt-2 text-sm border rounded-xl p-1 bg-slate-50">{el.title}</div>)}
       </div>
