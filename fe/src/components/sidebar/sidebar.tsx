819e7f343a3b3ff90400fcc194ddedde83b19d01
Jakob Lingel <jlingel68@gmail.com>
2024-11-21 22:53:10 +0100

    Clean up sidebar

--- fe/src/components/sidebar/sidebar.tsx
+++ fe/src/components/sidebar/sidebar.tsx
@@ -1,12 +1,9 @@
-import hamburgerIcon from "../../assets/icons/icons8-hamburger-50.png"
 import { useSidebarStore } from "../../store";
 import Cart from "../cart/cart";
-import UserMenu from "../userMenu";
 import { gsap } from 'gsap';
 
 export default function Sidebar() {
-  const toggleSidebarShown = useSidebarStore((state) => state.toggleSidebar);
-  let sidebarShown = useSidebarStore((state) => state.isShown);
+  const sidebarShown = useSidebarStore((state) => state.isShown);
 
   useSidebarStore.subscribe(() => {
     gsap.set('.sidebar', { x: 300 });
@@ -18,12 +15,6 @@ export default function Sidebar() {
       {sidebarShown ?
         <div className="p-4 bg-white shadow-md overflow-y-auto md:w-[500px] h-screen z-50">
           <div className="flex flex-col gap-4">
-            <div className="my-[6px] self-end">
-              <div className="flex items-center gap-2">
-                <UserMenu />
-                <button onClick={toggleSidebarShown}><img src={hamburgerIcon} width="30" alt="" /></button>
-              </div>
-            </div>
             <Cart />
           </div>
         </div>
