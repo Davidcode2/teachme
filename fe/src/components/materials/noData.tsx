3bbd0ace9cda0d7fa5b639707f35889f803bf111
Jakob Lingel <jlingel68@gmail.com>
2024-11-21 19:48:08 +0100

    Delay showing no data component by 100 ms

--- fe/src/components/materials/noData.tsx
+++ fe/src/components/materials/noData.tsx
@@ -1,9 +1,11 @@
 import meow_cat from '../../assets/white_cat_happy.webp';
 export default function NoData() {
-  return (
-    <div className="flex flex-col justify-center place-items-center h-[85vh]">
-      <img src={meow_cat} className="lg:max-w-[50%] max-w-[80%] rounded-xl"/>
-      <div className="relative bottom-40">Zeit zu shoppen!</div>
-    </div>
-  )
+  setTimeout(() => {
+    return (
+      <div className="flex flex-col justify-center place-items-center h-[85vh]">
+        <img src={meow_cat} className="lg:max-w-[50%] max-w-[80%] rounded-xl" />
+        <div className="relative bottom-40">Zeit zu shoppen!</div>
+      </div>
+    )
+  }, 100);
 }
