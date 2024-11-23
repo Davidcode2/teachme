c3fe50135d701dcd8b486453173538f0d0c783d5
Jakob Lingel <jlingel68@gmail.com>
2024-11-23 12:11:25 +0100

    Adjust sidebar position

--- fe/src/components/sidebar/sidebar.tsx
+++ fe/src/components/sidebar/sidebar.tsx
@@ -13,7 +13,7 @@ export default function Sidebar() {
   return (
     <div className="sidebar fixed right-0 w-screen md:w-[500px]">
       {sidebarShown ?
-        <div className="p-4 bg-white shadow-md overflow-y-auto md:w-[500px] h-screen z-50">
+        <div className="p-4 bg-white shadow-md overflow-y-auto md:w-[500px] pt-40 md:pt-24 h-screen z-50">
           <div className="flex flex-col gap-4">
             <Cart />
           </div>
