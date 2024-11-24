b227c9b41bb7b0de4242e17a29396618c6fa5d3c
Jakob Lingel <jlingel68@gmail.com>
2024-11-24 16:19:36 +0100

    Fix noData component show after timeout

--- fe/src/components/materials/noData.tsx
+++ fe/src/components/materials/noData.tsx
@@ -1,11 +1,22 @@
+import { useEffect, useState } from 'react';
 import meow_cat from '../../assets/white_cat_happy.webp';
 export default function NoData() {
-  setTimeout(() => {
-    return (
-      <div className="flex flex-col justify-center place-items-center h-[85vh]">
-        <img src={meow_cat} className="lg:max-w-[50%] max-w-[80%] rounded-xl" />
-        <div className="relative bottom-40">Zeit zu shoppen!</div>
-      </div>
-    )
-  }, 100);
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShow(true);
+    }, 100);
+  }, []);
+
+  return (
+    <>
+      {show &&
+        <div className="flex flex-col justify-center place-items-center h-[85vh]">
+          <img src={meow_cat} className="lg:max-w-[50%] max-w-[80%] rounded-xl" />
+          <div className="relative bottom-40">Zeit zu shoppen!</div>
+        </div>
+      }
+    </>
+  )
 }
