94525edd3e88f6a881cd15d7564f8951bfac1b91
Jakob Lingel <jlingel68@gmail.com>
2024-12-20 18:03:35 +0100

    Thinner hamburger

--- fe/src/components/cart/shoppingCartIcon.tsx
+++ fe/src/components/cart/shoppingCartIcon.tsx
@@ -19,18 +19,18 @@ function ShoppingCartIcon() {
           && <div className="absolute z-20 top-5 right-2 bg-emerald-500 text-white rounded-full w-5 h-5 flex justify-center items-center">{numberOfCartItems}</div>
         }
         <div
-          className="w-6 h-6 flex flex-col justify-between items-center cursor-pointer"
+          className="w-6 h-5 flex flex-col justify-between items-center cursor-pointer"
         >
           <div
-            className={`h-1 w-full bg-black rounded transition-transform duration-300 ease-in-out ${isOpen ? 'translate-y-2 rotate-45' : ''
+            className={`h-[3px] w-full bg-neutral-700 rounded transition-transform duration-300 ease-in-out ${isOpen ? 'translate-y-[9px] rotate-45' : ''
               }`}
           ></div>
           <div
-            className={`h-1 w-full bg-black rounded transition-opacity duration-300 ease-in-out ${isOpen ? 'opacity-0' : ''
+            className={`h-[3px] w-full bg-neutral-700 rounded transition-opacity duration-300 ease-in-out ${isOpen ? 'opacity-0' : ''
               }`}
           ></div>
           <div
-            className={`h-1 w-full bg-black rounded transition-transform duration-300 ease-in-out ${isOpen ? '-translate-y-3 -rotate-45' : ''
+            className={`h-[3px] w-full bg-neutral-700 rounded transition-transform duration-300 ease-in-out ${isOpen ? '-translate-y-2 -rotate-45' : ''
               }`}
           ></div>
         </div>
