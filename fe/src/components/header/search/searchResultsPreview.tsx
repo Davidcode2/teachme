a60930cbea53cd20e86f7d105def39c9b5aa9132
Jakob Lingel <jlingel68@gmail.com>
2024-12-23 16:56:21 +0100

    Fix search preview

--- fe/src/components/header/search/searchResultsPreview.tsx
+++ fe/src/components/header/search/searchResultsPreview.tsx
@@ -7,9 +7,8 @@ export default function SearchResultsPreview({ searchResults }: SearchResultsPre
   return (
     <div className="sm:grid grid-cols-2 mt-4">
       <div className="">
-        {searchResults.map((el: any, index: number) => <div key={index} className="inline-block mr-2 mt-2 text-sm border rounded-xl p-1 bg-slate-50">{el.title}</div>)}
+        {searchResults.map((el: any, index: number) => <div key={index} className="inline-block mr-2 mt-2 text-sm border rounded-xl p-1 bg-slate-50">{el.material.title}</div>)}
       </div>
-      <div className="text-3xl self-end justify-self-end">{searchResults.length}</div>
     </div>
   )
 }
