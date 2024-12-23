e59665804501e8cbe90fdb3c5423d55c61146616
Jakob Lingel <jlingel68@gmail.com>
2024-12-23 17:12:41 +0100

    Transparent no data cat

--- fe/src/components/materials/noData.tsx
+++ fe/src/components/materials/noData.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import meow_cat from '../../assets/white_cat_happy.webp';
+import meow_cat from '../../assets/white_cat_happy_transparent.webp';
 export default function NoData({ message, showImage }: { message?: string, showImage?: boolean }) {
   const [show, setShow] = useState(false);
 
@@ -13,8 +13,8 @@ export default function NoData({ message, showImage }: { message?: string, showI
     <>
       {show &&
         <div className="flex flex-col justify-center place-items-center h-[85vh]">
-      { showImage ?? <img src={meow_cat} className="lg:max-w-[50%] max-w-[80%] rounded-xl" /> }
-          <div className="relative bottom-40">{ message ?? "Zeit zu shoppen!" }</div>
+      { showImage && <img src={meow_cat} className="lg:max-w-[12%] max-w-[20%] rounded-xl" /> }
+          <div className="mt-4">{ message ?? "Zeit zu shoppen!" }</div>
         </div>
       }
     </>
