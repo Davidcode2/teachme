87983ab10b743d39d9d9f22d3aa8e43a5af1fa16
Jakob Lingel <jlingel68@gmail.com>
2024-12-27 21:20:01 +0100

    Fix possibly null

--- fe/src/actions/addMaterialAction.ts
+++ fe/src/actions/addMaterialAction.ts
@@ -4,7 +4,7 @@ import { useAccessTokenStore, useUserStore } from "../store";
 export default async function addMaterialAction({request}: { request: Request }) {
   const formData = await request.formData();
   const price = formData.get('price');
-  formData.set('price', price.toString().replace(/,/g, ''));
+  formData.set('price', price!.toString().replace(/,/g, ''));
   formData.append('userId', useUserStore.getState().user.id);
   await fetch('/api/materials', {
     method: 'POST',
