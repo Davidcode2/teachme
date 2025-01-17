fe32fe0a74947e5e5bfe856652cab1f0997f7ba1
Jakob Lingel <jlingel68@gmail.com>
2025-01-17 22:02:13 +0100

    Three part grid view on mobile

--- fe/src/components/card/card.tsx
+++ fe/src/components/card/card.tsx
@@ -34,7 +34,7 @@ function Card({
     <img
       src={image}
       onClick={togglePreview}
-      className="thumbnail object-top h-[40vh] cursor-pointer object-cover transition-transform duration-300 hover:scale-105 md:w-[400px] rounded-l-lg lg:w-[600px]"
+      className="thumbnail h-[20vh] sm:h-[40vh] cursor-pointer rounded-l-lg object-cover object-top transition-transform duration-300 hover:scale-105 md:w-[400px] lg:w-[600px]"
       alt="Thumbnail"
     />
   ) : (
@@ -79,36 +79,36 @@ function Card({
         id={materialWithThumbnail.material.id.toString()}
       >
         <BorderColorBlur>
-          <div className="grid sm:grid-cols-2 grid-cols-[30%_auto] text-sm">
-            <div className="overflow-hidden bg-white rounded-l-lg ">
+          <div className="overflow-hidden grid md:grid-cols-[2fr_3fr] grid-cols-[40%_auto] text-sm sm:grid-cols-2 bg-white rounded-lg">
+            <div className="overflow-hidden border-r sm:row-span-2">
               {imageElement}
             </div>
-            <div className="flex flex-1 flex-col gap-4 overflow-auto rounded-r-lg border-t border-slate-100 bg-white p-4 sm:p-10 border-l md:border-t-0">
+            <div className="flex flex-1 flex-col gap-4 overflow-auto rounded-r-lg border-slate-100 bg-white p-4 md:p-10 md:border-t-0">
               <div className="flex flex-col">
                 <div className="text-lg md:text-2xl">
                   {materialWithThumbnail.material.title}
                 </div>
                 <div>{materialWithThumbnail.material.description}</div>
               </div>
-              <p className="md:text-3xl text-emerald-500">
+              <p className="text-emerald-500 md:text-3xl">
                 {Number(materialWithThumbnail.material.price / 100).toFixed(2)}{" "}
                 €
               </p>
-              <div className="mt-auto flex">
-                <div className="self-center">
-                  <ActionButtons
-                    id={materialWithThumbnail.material.id.toString()}
-                    title={materialWithThumbnail.material.title}
-                    authorId={materialWithThumbnail.material.author_id}
-                    isMine={materialWithThumbnail.material.file_path}
-                  ></ActionButtons>
-                </div>
-                <div className="ml-auto self-end">
-                  <Author
-                    authorId={materialWithThumbnail.material.author_id}
-                    published={materialWithThumbnail.material.date_published}
-                  ></Author>
-                </div>
+            </div>
+            <div className="col-span-2 border-t sm:border-none sm:col-start-2 px-4 p-2 md:p-10 mt-auto flex">
+              <div className="self-center">
+                <ActionButtons
+                  id={materialWithThumbnail.material.id.toString()}
+                  title={materialWithThumbnail.material.title}
+                  authorId={materialWithThumbnail.material.author_id}
+                  isMine={materialWithThumbnail.material.file_path}
+                ></ActionButtons>
+              </div>
+              <div className="ml-auto self-end">
+                <Author
+                  authorId={materialWithThumbnail.material.author_id}
+                  published={materialWithThumbnail.material.date_published}
+                ></Author>
               </div>
             </div>
           </div>
