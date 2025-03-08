3e22165d70547d407fc225f52a5f5e405eaf77b2
Jakob Lingel <jlingel68@gmail.com>
2025-03-08 18:19:44 +0100

    Terminate ssl for keycloak

--- keycloak/start-keycloak.sh
+++ keycloak/start-keycloak.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Generate the keystore using the environment variable
-keytool -genkeypair -storepass "$KEYSTORE_PASSWORD" -storetype PKCS12 -keyalg RSA -keysize 2048 -dname "CN=server" -alias server -keystore conf/server.keystore
-
-# Start Keycloak with the keystore and password
+# Start Keycloak with the http-enabled
 /opt/keycloak/bin/kc.sh start \
-    --https-server-keystore-file=/opt/keycloak/conf/server.keystore \
-    --https-server-keystore-password="$KEYSTORE_PASSWORD" \
-    --https-server-cert-file=/opt/keycloak/conf/fullchain.pem \
-    --https-server-key-file=/opt/keycloak/conf/privkey.pem
+  --http-enabled=true \
+  --proxy-headers=xforwarded \
+  -–http-relative-path=/
