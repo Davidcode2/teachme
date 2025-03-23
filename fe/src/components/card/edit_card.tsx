bbf84b3c0575f03133be10fc0626478687d3915d
Jakob Lingel <jlingel68@gmail.com>
2025-03-23 16:18:36 +0100

    Adjust size of edit page

--- fe/src/components/card/edit_card.tsx
+++ fe/src/components/card/edit_card.tsx
@@ -6,6 +6,7 @@ import CheckMarkIcon from "../../assets/icons/icons8-checkmark-48.png";
 import CardService from "../../services/cardService.ts";
 import SpinnerGif from "../../assets/icons/icons8-spinnkreis.gif";
 import Price from "../materials/addMaterial/price.tsx";
+import BorderColorBlur from "../styling/borderColorBlur.tsx";
 
 function EditMaterial(): JSX.Element {
   const [materialWithThumbnail, setMaterialWithThumbnail] =
@@ -57,14 +58,14 @@ function EditMaterial(): JSX.Element {
   );
 
   const fileSelectedImageElement = image && (
-    <div className="group max-h-[250px] md:max-h-none relative flex items-center justify-center">
+    <div className="group relative flex max-h-[250px] items-center justify-center md:max-h-none">
       <div className="absolute z-10">
         {!file?.name && noFileSelectedElement}
         {file?.name && fileSelectedElement}
       </div>
       <img
         src={image}
-        className={`thumbnail cursor-pointer rounded-t-lg transition-transform duration-300 group-hover:scale-105 md:w-[400px] md:rounded-l-lg md:rounded-tr-none lg:w-[600px] ${file?.name ? "blur-xl" : "blur-sm"}`}
+        className={`thumbnail cursor-pointer rounded-t-lg transition-transform duration-300 group-hover:scale-105 md:w-[300px] md:rounded-l-lg md:rounded-tr-none lg:w-[400px] ${file?.name ? "blur-xl" : "blur-sm"}`}
         alt="Thumbnail"
       />
     </div>
@@ -79,68 +80,70 @@ function EditMaterial(): JSX.Element {
       ) : (
         <div
           id={materialWithThumbnail.material.id.toString()}
-          className="module-border-wrap m-4 rounded-lg shadow-lg transition-opacity duration-700 md:mx-10 md:mb-10"
+          className="m-4 lg:mx-auto rounded-lg shadow-lg transition-opacity duration-700 md:mx-20 md:mb-10 lg:w-2/3"
         >
-          <Form
-            method="patch"
-            encType="multipart/form-data"
-            className="flex flex-col md:flex-row"
-          >
-            <div className="overflow-hidden rounded-t-lg bg-white md:rounded-l-lg md:rounded-r-none">
-              <label htmlFor="file-edit-input">
-                {fileSelectedImageElement}
-              </label>
-              <input
-                id="file-edit-input"
-                className="hidden h-fit w-full min-w-0 rounded-md border border-slate-200 px-4 py-2"
-                type="file"
-                name="file"
-                onChange={handleFileChange}
-                accept="application/pdf"
-              />
-            </div>
-            <div className="flex flex-1 flex-col gap-4 overflow-auto rounded-b-lg border-t border-slate-100 bg-white p-10 md:rounded-r-lg md:rounded-bl-none md:border-l md:border-t-0">
-              <div className="flex flex-col gap-2">
+          <BorderColorBlur>
+            <Form
+              method="patch"
+              encType="multipart/form-data"
+              className="flex flex-col md:flex-row"
+            >
+              <div className="overflow-hidden rounded-t-lg bg-white md:rounded-l-lg md:rounded-r-none">
+                <label htmlFor="file-edit-input">
+                  {fileSelectedImageElement}
+                </label>
                 <input
-                  className="hidden"
-                  name="id"
-                  value={materialWithThumbnail.material.id.toString()}
-                  readOnly
-                />
-                <input
-                  className="max-w-fit rounded-lg border border-slate-200 p-2 text-2xl"
-                  type="text"
-                  placeholder={`${materialWithThumbnail.material.title}`}
-                  defaultValue={materialWithThumbnail.material.title}
-                  name="title"
-                />
-                <textarea
-                  className="max-w-fit rounded-lg border border-slate-200 p-2"
-                  cols={40}
-                  rows={5}
-                  placeholder={`${materialWithThumbnail.material.description}`}
-                  defaultValue={materialWithThumbnail.material.description}
-                  name="description"
-                />
-                <Price
-                  initialValue={materialWithThumbnail.material.price.toString()}
+                  id="file-edit-input"
+                  className="hidden h-fit w-full min-w-0 rounded-md border border-slate-200 px-4 py-2"
+                  type="file"
+                  name="file"
+                  onChange={handleFileChange}
+                  accept="application/pdf"
                 />
               </div>
-              <div className="mt-auto flex">
-                <div className="self-center">
-                  <button type="submit">
-                    <img src={CheckMarkIcon} className="w-8" alt="" />
-                  </button>
+              <div className="flex flex-1 flex-col gap-4 overflow-auto rounded-b-lg border-t border-slate-100 bg-white p-10 md:rounded-r-lg md:rounded-bl-none md:border-l md:border-t-0">
+                <div className="flex flex-col gap-2">
+                  <input
+                    className="hidden"
+                    name="id"
+                    value={materialWithThumbnail.material.id.toString()}
+                    readOnly
+                  />
+                  <input
+                    className="max-w-fit rounded-lg border border-slate-200 p-2 text-2xl"
+                    type="text"
+                    placeholder={`${materialWithThumbnail.material.title}`}
+                    defaultValue={materialWithThumbnail.material.title}
+                    name="title"
+                  />
+                  <textarea
+                    className="max-w-fit rounded-lg border border-slate-200 p-2"
+                    cols={40}
+                    rows={5}
+                    placeholder={`${materialWithThumbnail.material.description}`}
+                    defaultValue={materialWithThumbnail.material.description}
+                    name="description"
+                  />
+                  <Price
+                    initialValue={materialWithThumbnail.material.price.toString()}
+                  />
                 </div>
-                <div className="ml-auto self-end">
-                  <Author
-                    authorId={materialWithThumbnail.material.author_id}
-                    published={materialWithThumbnail.material.date_published}
-                  ></Author>
+                <div className="mt-auto flex">
+                  <div className="self-center">
+                    <button type="submit">
+                      <img src={CheckMarkIcon} className="w-8" alt="" />
+                    </button>
+                  </div>
+                  <div className="ml-auto self-end">
+                    <Author
+                      authorId={materialWithThumbnail.material.author_id}
+                      published={materialWithThumbnail.material.date_published}
+                    ></Author>
+                  </div>
                 </div>
               </div>
-            </div>
-          </Form>
+            </Form>
+          </BorderColorBlur>
         </div>
       )}
     </>
